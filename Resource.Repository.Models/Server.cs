688e98615a2dce08aa708e186cfcaaa85e450023
Yangtao Jiang <jiangyangtao1@qq.com>
2023-08-24 01:02:21 +0800

    Add Server

---
+++ Resource.Repository.Models/Server.cs
@@ -0,0 +1,13 @@
+﻿using Resource.Repository.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resource.Repository.Models
+{
+    public class Server : BaseEntity
+    {
+    }
+}
