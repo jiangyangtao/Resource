e5494127ef94a45b71374297f2a8ba2345224fbe
Yangtao Jiang <jiangyangtao1@qq.com>
2023-09-02 04:22:10 +0800

    Create DeployeController

---
+++ src/Resource.Application/Controllers/DeployeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Resource.Application.Controllers
+{
+    public class DeployeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
